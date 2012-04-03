e311760c09b87085b258d1930b8b04457c3618f1
William Menz <willmenz235@gmail.com>
2012-04-03 22:45:50 +0100

    Now have a basic menu with file select ability.

--- launder.py
+++ launder.py
@@ -27,26 +27,25 @@ except:
 class LaunderApp:
     def __init__(self):
         # Create main window
-        self.window_main = gtk.Window()
+        self.m_window_main = gtk.Window()
         
         # Initialise and connect signals
-        self.window_main.connect("destroy", gtk.main_quit)
-        self.window_main.set_default_size(600,400)
-        self.window_main.set_title("Launder GTK+")
+        self.m_window_main.connect("destroy", gtk.main_quit)
+        self.m_window_main.set_default_size(600,400)
+        self.m_window_main.set_title("Launder GTK+")
         
-        # Create the configurator container
-        self.config = gtk.HBox()
+        # Create the key widgets
+        self.m_toolbar = Toolbar()
+        self.m_mpl     = ContainerMPL()
         
-        # Create the matplotlib container
-        self.mpl = ContainerMPL()
+        # Create main vbox for toolbar
+        self.m_toolbar_vbox  = gtk.VBox(homogeneous=False)
+        self.m_toolbar_vbox.pack_start(self.m_toolbar.vbox, expand=False)
+        self.m_toolbar_vbox.pack_start(self.m_mpl.vbox)
+        self.m_window_main.add(self.m_toolbar_vbox)
         
-        # Create containers for layout
-        self.hbox = gtk.HBox()
-        self.hbox.pack_start(self.config)
-        self.hbox.pack_start(self.mpl.vbox)
-        self.window_main.add(self.hbox)
-        
-        self.window_main.show_all()
+        self.m_window_main.show_all()
+    
     
     def main(self):
         gtk.main()
@@ -67,9 +66,64 @@ class ContainerMPL:
         self.toolbar = NavToolbar(self.canvas, self.vbox)
         self.vbox.pack_start(self.toolbar, expand=False, fill=False)
 
-#class ContainerConfig:
-#    def __init__(self):
+# Definition of the program toobar widget
+class Toolbar:
+    def __init__(self):
+        # Create toolbar
+        self.vbox = gtk.VBox()
+        self.toolbar = gtk.Toolbar()
+        self.toolbar.set_style(gtk.TOOLBAR_ICONS)
+        self.toolbar.set_icon_size(4)
         
+        # Add key buttons
+        self.b_open = gtk.ToolButton(gtk.STOCK_OPEN)
+        self.b_open.connect("clicked", self.chooseFile, )
+        self.b_quit = gtk.ToolButton(gtk.STOCK_QUIT)
+        self.b_quit.connect("clicked", gtk.main_quit)
+        
+        # Insert buttons
+        self.toolbar.insert(self.b_open, 0)
+        self.toolbar.insert(self.b_quit, 1)
+        
+        self.vbox.pack_start(self.toolbar, fill=False)
+    
+    def chooseFile(self, data=None):
+        # Check PyGTK version
+        if gtk.pygtk_version < (2,3,90):
+           print("PyGtk 2.3.90 or later required!")
+           raise SystemExit
+        
+        dialog = gtk.FileChooserDialog("Select file or path..",
+                               None,
+                               gtk.FILE_CHOOSER_ACTION_OPEN,
+                               (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
+                                gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+        dialog.set_default_response(gtk.RESPONSE_OK)
+        
+        # Add file filters to dialog
+        filter = gtk.FileFilter()
+        filter.set_name("All files")
+        filter.add_pattern("*")
+        dialog.add_filter(filter)
+
+        filter = gtk.FileFilter()
+        filter.set_name("MOPS outputs")
+        filter.add_pattern("*.csv")
+        filter.add_pattern("*.sav")
+        filter.add_pattern("*.aux")
+        filter.add_pattern("*.sim")
+        dialog.add_filter(filter)
+        
+        # Now run the chooser!
+        fname = dialog.run()
+        
+        # Check the response
+        if fname == gtk.RESPONSE_OK:
+            print dialog.get_filename(), 'selected'
+        elif fname == gtk.RESPONSE_CANCEL:
+            print 'Closed, no files selected'
+        self.m_selected_file = fname
+        dialog.destroy()
 
 if __name__ == "__main__":
     app = LaunderApp()
