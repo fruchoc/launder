6e339394d7bd54c5e09a5e9364584546131291c7
William Menz <willmenz235@gmail.com>
2012-11-22 09:50:17 +0000

    Improve robustness of parser.

--- structure/lparser.py
+++ structure/lparser.py
@@ -4,45 +4,78 @@ import csv
 def DSVParser(fname):
     # Parses a DSV file and returns a dictionary of {"header": [values]}
     
-    s = csv.Sniffer()
-    f = open(fname, "rb")
-    # Use three lines of file for context
-    l += f.readline() + f.readline() + f.readline()
-    f.close()
-    d = s.sniff(f)
-    hh = s.has_header(f)
     
-    cstr = csv.DictReader(open(fname, 'r'), delimiter=d.delimiter)
     
-    # Create dictionaries to hold the fields
-    results = {}
-    if hh:
-        for n in cstr.fieldnames:
-            results[n] = []
-    else:
-        for i in range(0, len(cstr.fieldnames)):
-            results["line"+str(i)] = []
-    
-    for line in cstr:
-        for n in cstr.fieldnames:
-            results[n].append(float(line[n]))
-    
-    return results
+
+
 
 class LParser:
-    # Generic parsing of DSV files.
+    # Generic parsing of DSV files. Generates a dictionary of lists 
+    # (columns of DSV files).
     def __init__(self, fname):
         self.fname = fname
     
+    def __check(self, fname):
+        # Checks the dialect and whether the file has headers.
+        s = csv.Sniffer()
+        
+        try:
+            f = open(fname, "rb")
+        except IOError:
+            print "Couldn't open", fname
+            return None
+        else:
+            # Use three lines of file for context
+            l += f.readline() + f.readline() + f.readline()
+            f.close()
+            d = s.sniff(f)
+            hh = s.has_header(f)
+            
+            return {"dialect": d, "has_headers": hh}
+    
+    def __preLoad(self, fname):
+        # First get any properties of the DSV file
+        p = self.__check(self.fname)
+        
+        # Create dictionaries to hold the fields
+        r = {}
+        cstr = csv.DictReader(open(fname, 'r'), 
+                delimiter=p["dialect"].delimiter)
+        
+        # Generate the keys
+        keys = []
+        if p["has_headers"]:
+            keys = cstr.fieldnames
+        else:
+            for i in range(0, len(cstr.fieldnames)):
+                keys.append("col"+str(i))
+        
+        return (keys, cstr)
+    
     def get(self):
-        return DSVParser(fname)
+        (keys, cstr) = self.__preLoad(fname)
+        
+        # Load the data from the lines
+        for line in cstr:
+            for k in keys:
+                r[k].append(float(line[k]))
+        
+        return {"data": r, "keys": keys}
 
 class PSLParser(LParser):
-    # For the parsing of PSL files.
+    # For the parsing of PSL files. This gets a list of dictionaries 
+    # (rows of PSL files), and their keys.
     def __init__(self, name):
         super(LParser, self).__init__(fname)
         
     def get(self):
-        # Get a dictionary of results.
-        r = super(LParser, self).get()
+        (keys, cstr) = self.__preLoad(fname)
         
+        # Load the data from the lines:
+        for line in cstr:
+            d = {}
+            for k in keys:
+                d[k] = line[k]
+            r.append(d)
+        
+        return {"data": r, "keys": keys} 
